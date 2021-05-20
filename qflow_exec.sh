a703b4e573a1bac7047b318051a5b286b5a4ff84
Egor <pifagor36@yandex.ru>
2021-05-20 15:50:32 +0300

    migrate

--- qflow_exec.sh
+++ qflow_exec.sh
@@ -7,8 +7,8 @@
 # /usr/local/share/qflow/scripts/placement.sh -d /home/dos/qflow_diff counter || exit 1
 # /usr/local/share/qflow/scripts/vesta.sh  /home/dos/qflow_diff counter || exit 1
 # /usr/local/share/qflow/scripts/router.sh /home/dos/qflow_diff counter || exit 1
-/usr/local/share/qflow/scripts/vesta.sh  -d /home/dos/qflow_diff counter || exit 1
-# /usr/local/share/qflow/scripts/migrate.sh /home/dos/qflow_diff counter || exit 1
+# /usr/local/share/qflow/scripts/vesta.sh  -d /home/dos/qflow_diff counter || exit 1
+/usr/local/share/qflow/scripts/migrate.sh /home/dos/qflow_diff counter || exit 1
 # /usr/local/share/qflow/scripts/drc.sh /home/dos/qflow_diff counter || exit 1
 # /usr/local/share/qflow/scripts/lvs.sh /home/dos/qflow_diff counter || exit 1
 # /usr/local/share/qflow/scripts/gdsii.sh /home/dos/qflow_diff counter || exit 1
