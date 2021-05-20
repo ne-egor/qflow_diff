b1f29fc30f97dd214e75f8f6e496cb031b9223b5
Egor <pifagor36@yandex.ru>
2021-05-20 15:49:53 +0300

    backanno

--- qflow_exec.sh
+++ qflow_exec.sh
@@ -6,8 +6,8 @@
 # /usr/local/share/qflow/scripts/synthesize.sh /home/dos/qflow_diff counter /home/dos/qflow_diff/source/counter.v || exit 1
 # /usr/local/share/qflow/scripts/placement.sh -d /home/dos/qflow_diff counter || exit 1
 # /usr/local/share/qflow/scripts/vesta.sh  /home/dos/qflow_diff counter || exit 1
-/usr/local/share/qflow/scripts/router.sh /home/dos/qflow_diff counter || exit 1
-# /usr/local/share/qflow/scripts/vesta.sh  -d /home/dos/qflow_diff counter || exit 1
+# /usr/local/share/qflow/scripts/router.sh /home/dos/qflow_diff counter || exit 1
+/usr/local/share/qflow/scripts/vesta.sh  -d /home/dos/qflow_diff counter || exit 1
 # /usr/local/share/qflow/scripts/migrate.sh /home/dos/qflow_diff counter || exit 1
 # /usr/local/share/qflow/scripts/drc.sh /home/dos/qflow_diff counter || exit 1
 # /usr/local/share/qflow/scripts/lvs.sh /home/dos/qflow_diff counter || exit 1
