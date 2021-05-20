41e63f3222283f06ff6ac7590732690459bc1e4a
Egor <pifagor36@yandex.ru>
2021-05-20 15:47:33 +0300

    place

--- qflow_exec.sh
+++ qflow_exec.sh
@@ -3,8 +3,8 @@
 # qflow exec script for project /home/dos/qflow_diff
 #-------------------------------------------
 
-/usr/local/share/qflow/scripts/synthesize.sh /home/dos/qflow_diff counter /home/dos/qflow_diff/source/counter.v || exit 1
-# /usr/local/share/qflow/scripts/placement.sh -d /home/dos/qflow_diff counter || exit 1
+# /usr/local/share/qflow/scripts/synthesize.sh /home/dos/qflow_diff counter /home/dos/qflow_diff/source/counter.v || exit 1
+/usr/local/share/qflow/scripts/placement.sh -d /home/dos/qflow_diff counter || exit 1
 # /usr/local/share/qflow/scripts/vesta.sh  /home/dos/qflow_diff counter || exit 1
 # /usr/local/share/qflow/scripts/router.sh /home/dos/qflow_diff counter || exit 1
 # /usr/local/share/qflow/scripts/vesta.sh  -d /home/dos/qflow_diff counter || exit 1
