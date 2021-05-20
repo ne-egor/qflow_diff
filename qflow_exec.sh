f6412547383fb748fca583d7fb965dc8916415d5
Egor <pifagor36@yandex.ru>
2021-05-20 15:52:03 +0300

    lvs

--- qflow_exec.sh
+++ qflow_exec.sh
@@ -9,8 +9,8 @@
 # /usr/local/share/qflow/scripts/router.sh /home/dos/qflow_diff counter || exit 1
 # /usr/local/share/qflow/scripts/vesta.sh  -d /home/dos/qflow_diff counter || exit 1
 # /usr/local/share/qflow/scripts/migrate.sh /home/dos/qflow_diff counter || exit 1
-/usr/local/share/qflow/scripts/drc.sh /home/dos/qflow_diff counter || exit 1
-# /usr/local/share/qflow/scripts/lvs.sh /home/dos/qflow_diff counter || exit 1
+# /usr/local/share/qflow/scripts/drc.sh /home/dos/qflow_diff counter || exit 1
+/usr/local/share/qflow/scripts/lvs.sh /home/dos/qflow_diff counter || exit 1
 # /usr/local/share/qflow/scripts/gdsii.sh /home/dos/qflow_diff counter || exit 1
 # /usr/local/share/qflow/scripts/cleanup.sh /home/dos/qflow_diff counter || exit 1
 # /usr/local/share/qflow/scripts/display.sh /home/dos/qflow_diff counter || exit 1
