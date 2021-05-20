01044c8047f1cd17a5b2305afa6bcaa61dfcaffd
Egor <pifagor36@yandex.ru>
2021-05-20 15:48:27 +0300

    sta

--- qflow_exec.sh
+++ qflow_exec.sh
@@ -4,8 +4,8 @@
 #-------------------------------------------
 
 # /usr/local/share/qflow/scripts/synthesize.sh /home/dos/qflow_diff counter /home/dos/qflow_diff/source/counter.v || exit 1
-/usr/local/share/qflow/scripts/placement.sh -d /home/dos/qflow_diff counter || exit 1
-# /usr/local/share/qflow/scripts/vesta.sh  /home/dos/qflow_diff counter || exit 1
+# /usr/local/share/qflow/scripts/placement.sh -d /home/dos/qflow_diff counter || exit 1
+/usr/local/share/qflow/scripts/vesta.sh  /home/dos/qflow_diff counter || exit 1
 # /usr/local/share/qflow/scripts/router.sh /home/dos/qflow_diff counter || exit 1
 # /usr/local/share/qflow/scripts/vesta.sh  -d /home/dos/qflow_diff counter || exit 1
 # /usr/local/share/qflow/scripts/migrate.sh /home/dos/qflow_diff counter || exit 1
